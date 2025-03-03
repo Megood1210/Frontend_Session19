f12c291d841feabd05866f639aebbdc8b6a16d22
Megood1210 <xuanvinhle06@gmail.com>
2025-03-03 20:07:05 +0700

    Upload bài tập 1

---
+++ ss19ex1.js
@@ -0,0 +1,41 @@
+let months = +prompt("Nhap mot thang");
+switch (months) {
+    case 1:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    case 2:
+        document.write(`Tháng ${months} có 28 hoặc 29 ngày năm nhuận`);
+        break;
+    case 3:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    case 4:
+        document.write(`Tháng ${months} có 30 ngày`);
+        break;
+    case 5:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    case 6:
+        document.write(`Tháng ${months} có 30 ngày`);
+        break;
+    case 7:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    case 8:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    case 9:
+        document.write(`Tháng ${months} có 30 ngày`);
+        break;
+    case 10:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    case 11:
+        document.write(`Tháng ${months} có 30 ngày`);
+        break;
+    case 12:
+        document.write(`Tháng ${months} có 31 ngày`);
+        break;
+    default:
+        break;
+}
